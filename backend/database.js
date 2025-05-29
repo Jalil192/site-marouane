675b71535abda98728a4204bd7d1a7a5b4e447d1
Jalil192 <jalil92150@gmail.com>
2025-05-29 03:39:19 +0200

    Initial commit backend Umanity

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,8 @@
-// backend/database.js
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://ihobtuplijggiwmjxewx.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlob2J0dXBsaWpnZ2l3bWp4ZXd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzODQ3OTcsImV4cCI6MjA2Mzk2MDc5N30.vMHhk3Yox8eqhLtiYAgLSr6LNuOgfD2PCwlk6MOXit0';
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = supabase;
